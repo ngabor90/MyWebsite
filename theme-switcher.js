0eeff9b78da077a680f26ab6f435edc1798c4c0a
ngabor90 <n.gabor90@gmail.com>
2024-09-15 15:28:40 +0200

    dark-mode

---
+++ theme-switcher.js
@@ -0,0 +1,32 @@
+const doc = document.getElementsByTagName("html")[0];
+const light = document.getElementById("light");
+const dark = document.getElementById("dark");
+
+const themeSwitcher = theme => {
+    if (theme == "light") {
+        light.style.display = "none"
+        dark.style.display = "block"
+        doc.classList.add("dark-mode")
+        localStorage.setItem("theme", "dark")
+        return
+    }
+    else {
+        light.style.display = "block"
+        dark.style.display = "none"
+        doc.classList.remove("dark-mode")
+        localStorage.setItem("theme", "light")
+    }
+}
+
+light.addEventListener("click", () => themeSwitcher("light"))
+dark.addEventListener("click", () => themeSwitcher("dark"))
+
+if (localStorage.getItem("theme")) {
+    themeSwitcher(localStorage.getItem("theme") == "dark" ? "light" : "dark")
+}
+
+if (!localStorage.getItem("theme")) {
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? themeSwitcher("light")
+        : themeSwitcher("dark")
+}
